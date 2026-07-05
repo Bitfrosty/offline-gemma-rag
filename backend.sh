9ea74cf53b041d22cca8f235526842efb4c58567
Bitfrosty <jrsubr@gmail.com>
2026-07-04 20:10:17 -0400

    added ability to access from local network on outside devices

--- backend.sh
+++ backend.sh
@@ -5,11 +5,11 @@ mkdir -p ./pids ./logs
 source .venv/bin/activate
 
 uvicorn server:app \
-  --host 127.0.0.1 \
+  --host 0.0.0.0 \
   --port 3000 \
   --reload \
   > ./logs/backend.log 2>&1 &
 
 echo $! > ./pids/backend.pid
 
-echo "Backend started on http://127.0.0.1:3000"
+echo "Backend started on http://0.0.0.0:3000"
